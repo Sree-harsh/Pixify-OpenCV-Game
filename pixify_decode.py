4c7e888b67f2349913d73388d032bd29481e6a66
Sree-harsh <sree16062003@gmail.com>
2024-03-03 11:41:42 +0530

    Game Ready

--- pixify_decode.py
+++ pixify_decode.py
@@ -5,37 +5,27 @@ import sys
 import time
 import random
 
+'''
+Guidelines :- 1) You are not allowed to import libraries other than those already mentioned
+              2) You are allowed to code only in the function given below . Code present anywhere else would be ignored by the code verifying exec.
+              3) You must code in such a way that the code inside the function is robust for all test cases.
+              4) The code verifying exectubale would iterate over the test cases and call this function with one test image at a time. 
+'''
+
 def decode(image):
-    yellow_squares = 0
-    red_squares = 0
-    shape_squares = 0
+    '''
+    Description:- This function takes in image as the input (a numpy array) and returns the character embedded in the image
+    For example : if yellow squares = 4 , red squares = 3 , number of shapes containing shapes = 5 . Then the correct character to be returned would be (4*2 + 3*1 + 5) which is p. 
+    Note :- if the value comes out to be 32 then the function should return an empty single space " ".
+    '''
 
-    for x in range(100,700,100):
-        for y in range(100,700,100):
-            pix = image[y,x]
-            # print(pix)
-            if np.array_equal(pix,np.array([0,255,255])):#pix.any() == np.array([0,255,255]):
-                yellow_squares+=1
-            elif np.array_equal(pix,np.array([0,0,255])):
-                red_squares+=1
 
-    for x in range(150,650,100):
-        for y in range(150,650,100):
-            if not np.array_equal(image[x-20,y-20],np.array([255,255,255])):
-                shape_squares+=1
-                continue
-            elif not np.array_equal(image[x+20,y-20],np.array([255,255,255])):
-                shape_squares+=1
-                continue
-            elif not np.array_equal(image[x-20,y+20],np.array([255,255,255])):
-                shape_squares+=1
-                continue
-            elif not np.array_equal(image[x+20,y+20],np.array([255,255,255])):
-                shape_squares+=1
-                continue
-        pass
-    if yellow_squares*2+red_squares+shape_squares == 32:
-        return " "
-    else:
-        return chr(yellow_squares*2+red_squares+shape_squares+96)
+    ############ Enter your Code Here #################
+    character = " "
+
+
+
+    ###################################################
+    return character
+
     
